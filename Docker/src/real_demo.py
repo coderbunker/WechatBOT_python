e19ba25ce0530af7b23f2864311d4267851e0de8
terrykim1211 <terrykim1211@naver.com>
2019-07-19 14:50:08 +0800

    Update real_demo.py

--- Docker/src/real_demo.py
+++ Docker/src/real_demo.py
@@ -1,7 +1,7 @@
 import time, datetime, requests, itchat
 from itchat.content import *
 import logging
-itchat.auto_login()
+itchat.auto_login(enableCmdQR = True)
 
 #############################################################Try using  Content = input(msg.text)
 
